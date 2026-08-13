e9d301278c3b19c919f306402eb47fadaf9f5fde
Belizario <62390637+belizariomon@users.noreply.github.com>
2026-08-13 12:29:46 -0300

    Fix/conversation error (#16)
    * wip: logs for watching
    
    * wip: log for backend conversation
    
    * wip: log for backend conversation
    
    * fix: log lenght of open_api_ia
    
    * fix: openia connection, more logs
    
    * fix: changin functionality for transcribe

--- backend/src/services/openai-client.ts
+++ backend/src/services/openai-client.ts
@@ -1,5 +1,8 @@
-import OpenAI from 'openai'
+import OpenAI from 'openai';
+import { toFile } from 'openai';
 
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-})
+});
+
+export { toFile };
